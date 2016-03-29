f2df10a1eb07044ee1b10b9c8b4800906904a0c0
Yaroslav <ibragimovyaroslav@gmail.com>
2016-03-29 15:01:57 +0300

    update 29.03

--- bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001459146696';
-$dateexpire = '001459150356';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1459146696;}';
-return true;
-?>
