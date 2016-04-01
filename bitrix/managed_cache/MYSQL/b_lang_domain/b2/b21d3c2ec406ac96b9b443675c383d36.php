04365b1dd29695412f92fb1d2fcd998207650be9
Yaroslav <ibragimovyaroslav@gmail.com>
2016-04-01 14:33:06 +0300

    update 1.04

--- bitrix/managed_cache/MYSQL/b_lang_domain/b2/b21d3c2ec406ac96b9b443675c383d36.php
+++ bitrix/managed_cache/MYSQL/b_lang_domain/b2/b21d3c2ec406ac96b9b443675c383d36.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001459404892';
-$dateexpire = '001459408492';
+$datecreate = '001459509663';
+$dateexpire = '001459513263';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:2:"s1";a:1:{i:0;a:2:{s:6:"LD_LID";s:2:"s1";s:9:"LD_DOMAIN";s:13:"gkexchange.ru";}}}}';
 return true;
 ?>
